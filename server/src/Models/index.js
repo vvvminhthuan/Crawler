e48c5ab8b0867f2da53759eb05b520122cd81d06
MPCH-006-PC\MPCH-006 <hoang_minh_thuan@managerment-partners.co.jp>
2022-02-08 09:20:36 +0900

    update connect db with sequelize

--- server/src/Models/index.js
+++ server/src/Models/index.js
@@ -6,18 +6,17 @@ const ModelGroupUsers = require('./ModelGroupUsers')
 const ModelMessages = require('./ModelMessages')
 const ModelRoles = require('./ModelRoles')
 const ModelUsers = require('./ModelUsers')
+const Model = {}
 
-const Model = (() => {
-    ModelUsers.belongsTo(ModelRoles, {foreignKey: 'roleId'})
-    return {
-        ModelFiles,
-        ModelGroups,
-        ModelGroupUsers,
-        ModelMessages,
-        ModelRoles,
-        ModelUsers,
-        transaction: sequelize.transaction()
-    }
-})()
+Model.sequelize = sequelize
 
-module.exports = Model 
+Model.ModelFiles = ModelFiles,
+Model.ModelGroups = ModelGroups,
+Model.ModelGroupUsers = ModelGroupUsers,
+Model.ModelMessages = ModelMessages,
+Model.ModelRoles = ModelRoles,
+Model.ModelUsers = ModelUsers,
+// Association
+Model.ModelUsers.belongsTo(ModelRoles, {foreignKey: 'roleId'})
+
+module.exports = {...Model} 

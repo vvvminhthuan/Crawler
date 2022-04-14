39a260456187eef63bce1ea5b3ccc3f20212bc45
MPCH-006-PC\MPCH-006 <hoang_minh_thuan@managerment-partners.co.jp>
2022-04-14 16:03:20 +0900

    create select widget

--- client/components/Widgets/SelectWidgets.tsx
+++ client/components/Widgets/SelectWidgets.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
+type Prop = {
+    className?: string,
+    placeholder?: string,
+    name?: string,
+    value?: string,
+    data?: any[],
+    option?: {
+        multi?: boolean,
+        hasSearch?: boolean,
+        fieldName?: string,
+        fieldValue?: string
+    }
+}
+const SelectWidgets: React.FC<Prop> = ({className, placeholder, name, value, data, option}) => {
+    option.hasSearch = option?.hasSearch?? false
+    option.multi = option?.multi?? false
+    option.fieldName = option?.fieldName?? ''
+    option.fieldValue = option?.fieldValue?? ''
 
-const SelectWidgets = () => {
-  return (
-    <div>SelectWidgets</div>
-  )
+    const [optionSelect, setOptionSelect] = useState([])
+
+    return (
+        <div className={`select-widget ${className}`}>
+            <div className="select-field flex-r">
+                <div className="label-option flex-r">
+                    {
+                        optionSelect.map(i=>{
+                            <div className="label-item">
+                                <span className="item-name">{i[option.fieldName]}</span>
+                                <span className="item-remove">X</span>
+                            </div>
+                        })
+                    }
+                </div>
+                <div className="select-input">
+                    <input type="text" value={value} placeholder={placeholder} />
+                </div>
+            </div>
+            
+            <div className="list-option flex-c">
+                {
+                    data.map((item, e) => {
+                        <label htmlFor={`option-${e}`} className="option">
+                            <input type="checkbox" name={name} id={`option-${e}`} value={item[option.fieldValue]}/>
+                            {item[option.fieldName]}
+                        </label>
+                    })
+                }
+            </div>
+        </div>
+    )
 }
 
 export default SelectWidgets
